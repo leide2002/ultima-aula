b181d4cf2131bd32f04b27677c8a41c6f34bdd3f
unknown <laidy34x@gmail.com>
2025-04-29 16:43:20 -0300

    teste04

--- test04.py
+++ test04.py
@@ -1,4 +1,12 @@
-nota=int(input("not:"))
-for in range(5):
-    if nota<=5:
-
+nota=[""]*5
+soma =0
+contador=0
+for i in range(len(nota)):
+    nota[i]=float(input("digite a nota:"))
+for j in range(5):
+    soma=soma+nota[j]
+media=soma/5
+for x in range(5):
+    if nota[i]>media:
+        contador=contador+1
+    print(f"{contador}notas maiores que a {media}")
